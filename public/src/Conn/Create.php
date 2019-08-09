7ba33aea31e9999ef854bc403e3b9860d16dc079
Edinei J. Bauer <galera.org@gmail.com>
2019-08-08 23:30:36 -0300

    create and update fix dash

--- public/src/Conn/Create.php
+++ public/src/Conn/Create.php
@@ -93,7 +93,7 @@ class Create extends Conn
         foreach ($this->dados as $key => $dado)
             $this->dadosName[str_replace('-', '_', \Helpers\Check::name($key))] = $dado;
 
-        $this->create = "INSERT INTO {$this->tabela} ({$Fileds}) VALUES ({:" . implode(', :', array_keys($this->dadosName)) . "})";
+        $this->create = "INSERT INTO {$this->tabela} ({$Fileds}) VALUES (:" . implode(', :', array_keys($this->dadosName)) . ")";
     }
 
     //Obtém a Conexão e a Syntax, executa a query!
